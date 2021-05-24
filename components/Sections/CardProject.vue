0bd2f1aac80d3c0b4ddd55c276b9c8a3deb35594
Gregory Anne <englishblazere@free.fr>
2021-05-24 13:39:28 +0000

    I've added a collection of personal projects

--- components/Sections/CardProject.vue
+++ components/Sections/CardProject.vue
@@ -5,13 +5,14 @@
       style="width: 80%; text-align: center"
     >
       <a :href="projectInfo.siteUrl" >
-        <img class="card-img-top" :src="projectInfo.image" alt="Kelawar"
+        <img class="card-img-top" :src="projectInfo.image" alt="project image"
       /></a>
       <div class="card-body">
         <h5 class="card-title">{{ projectInfo.name }}</h5>
         <p class="card-text">
           {{ projectInfo.description }}
         </p>
+        <small><a :href="projectInfo.repositoryUrl" >this code is open source!</a></small>
       </div>
     </div>
   </div>
